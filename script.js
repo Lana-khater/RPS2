eb310977bb33dc074808a2f025cfeec2e00c5d44
WALID ALRIFAI <78854936+alwalidalrifai@users.noreply.github.com>
2021-05-13 11:44:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,91 @@
-$(window).load(function() {
-  
-  var movesContainer = $('#moves'),
-      playerCount = $('.Player-count'),
-      computerCount = $('.Computer-count');
-  
-  $('#moves li').on("click", function() {
-    var yourMove = $(this).data('move');
-    processMoves(yourMove);
-    
-    var icon = $(this).find('.fa');
-    icon.addClass('animating');
-    
-    var s = setTimeout(function(icon) {
-      $('#moves li .fa').removeClass('animating');
-      clearTimeout();
-    }, 5000);
-  });
-  
-});
+let player,
+    computer,
+    playerIcon,
+    computerIcon,
+    winCombinations = [
+  ['paper', 'rock'],
+  ['rock', 'scissors'],
+  ['scissors', 'paper']  
+];
 
-function processMoves(yourMove) {
-  var opponentMove = generateRandomOpponentMove();
-  calculateWinner(yourMove,opponentMove);
+
+//setup the game
+function playGame(choice) {
+  $('#pick').hide();
+  $('#game').show();
+  player = choice;
+   
+  $('.fight').show();
+  $('.playAgain').hide();
+  $('#computer').html(getBackLizardHands('', 'Computer'));
+  $('#player').html(getBackLizardHands('reverse', 'Player'));
 }
 
-function generateRandomOpponentMove() {
-  var availableMoves = ['rock','paper','scissors'],
-      randomNumber = Math.floor(Math.random() * availableMoves.length),
-      opponentMove = availableMoves[randomNumber];
-  
-  return opponentMove;
+function playAgain() {
+  $('#pick').show();
+  $('#game').hide();
+  $('#win').text('');
 }
 
-function calculateWinner(yourMove, opponentMove) {
-  switch(yourMove) {
-    case 'rock':
-      if(opponentMove === 'rock') {
-        return 'draw';
-      } else if (opponentMove === 'scissors') {
-        youWin();
-      } else {
-        youLose();
-      }
-      break;
-    case 'paper':
-      if(opponentMove === 'paper') {
-        return 'draw';
-      } else if (opponentMove === 'rock') {
-        youWin();
-      } else {
-        youLose();
-      }
-      break;
-    case 'scissors':
-      if(opponentMove === 'scissors') {
-        return 'draw';
-      } else if (opponentMove === 'paper') {
-        youWin();
-      } else {
-        youLose();
-      }
-      break;
+function getComputer() {
+  let min = 0;
+  let max = 2;
+  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  switch (randomNumber) {
+    case 0:
+        computer = "rock";
+        break;
+    case 1:
+        computer = "paper";
+        break;
+    case 2:
+        computer = "scissors";
   }
+  return buildIcon(computer, 'Computer');
 }
 
-function youWin() {
-  var playerScore = $('.Player-count');
-  var playerScoreUpdated = parseInt(playerScore.text()) + 1;
-  playerScore.text(playerScoreUpdated);
+function buildIcon(icon, player) {
+  completeIcon = "<h3 class='player'>" + player + "</h3>" +
+    "<i class='fa fa-hand-" +
+    icon + "-o' aria-hidden='true'></i>";
+  completeIcon += "<h3>" + icon + "</h3>" 
+  return completeIcon;
 }
 
-function youLose() {
-  var computerScore = $('.Computer-count');
-  var computerScoreUpdated = parseInt(computerScore.text()) + 1;
-  computerScore.text(computerScoreUpdated);
+function getBackLizardHands(reverse,player) {
+  var lizardHand = "<h3 class='player'>" + player + "</h3>" +
+      "<i class='fa fa-hand-lizard-o " + reverse +
+      "' aria-hidden='true'></i>";
+  console.log(lizardHand);
+  return lizardHand;
+}
+
+function checkWin() {
+  if (player == computer) {
+    $('#win').text('Tie');
+  } else {
+    for (var i = 0; i < winCombinations.length; i++) {
+      if (winCombinations[i][0] == player) {
+        if (winCombinations[i][1] == computer) {
+          $('#win').text('Player Won');
+        } else {
+          $('#win').text('Computer Won');
+        }
+      }
+    }
+    
+  }
+  
+}
+
+function fight() {
+ 
+  playerIcon = buildIcon(player,'Player');
+  $('#player').html(playerIcon).css('textTransform', 'capitalize');
+  computerIcon = getComputer();
+  $('#computer').html(computerIcon).css('textTransform', 'capitalize');
+  $('.fight').hide();
+  $('.playAgain').show();
+  checkWin();
+  
 }
